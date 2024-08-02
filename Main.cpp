c7f5985198b27f283b892547779307e06b4218a8
b21phiro <philip.rosenqvist@outlook.com>
2024-08-02 11:38:48 +0200

    Removed shitty files

--- Main.cpp
+++ Main.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 
-int main() {
-    std::vector<std::shared_ptr<Land>> board;
-
-    board.push_back(std::make_shared<Property>("Boardwalk", 400, 50));
-    board.push_back(std::make_shared<ActionLand>("Go to Jail"));
-    board.push_back(std::make_shared<Property>("Park Place", 350, 35));
-    board.push_back(std::make_shared<ActionLand>("Community Chest"));
-
-    for (const auto& land : board) {
-        std::cout << "Landing on: " << land->getName() << std::endl;
-        land->action();
-        std::cout << std::endl;
-    }
-
+int main() 
+{
     return 0;
 }
