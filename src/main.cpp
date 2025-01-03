c1cd8b9962d7d116e2b657311a538890d39c036c
b21phiro <philip.rosenqvist@outlook.com>
2025-01-03 12:57:59 +0100

    Added resource folder

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,38 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), WINDOW_TITLE);
-    window.setFramerateLimit(144);
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
 
+    // Load a sprite to display
+    const sf::Texture texture("C:/Users/Phili/CLionProjects/Mononpoly/src/resources/textures/alma-liten-1-1.jpg");
+    sf::Sprite sprite(texture);
+
+    // Create a graphical text to display
+    const sf::Font font("C:/Users/Phili/CLionProjects/Mononpoly/src/resources/fonts/Dosis-Regular.ttf");
+    sf::Text text(font, "Hello SFML", 50);
+
+    // Start the game loop
     while (window.isOpen())
     {
+        // Process events
         while (const std::optional event = window.pollEvent())
         {
+            // Close window: exit
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
 
+        // Clear screen
         window.clear();
+
+        // Draw the sprite
+        window.draw(sprite);
+
+        // Draw the string
+        window.draw(text);
+
+        // Update the window
         window.display();
     }
 
